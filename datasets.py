1d5cd9055094c18cfe1b8be080af9c9eddc0dc5f
matthewli725 <matthewli831@gmail.com>
2025-05-28 11:00:19 -0700

    combined hotdog and cat datasets, tested on three class resnet18

--- datasets.py
+++ datasets.py
@@ -5,20 +5,18 @@ import numpy as np
 from pathlib import Path
 from torch.utils.data import Dataset
 from torchvision import transforms
+from torchvision import datasets, transforms
+from torch.utils.data import DataLoader
 from PIL import Image
 import torch
+import os
 
 
 def get_hotdog_train_test_paths():
     base = Path(kagglehub.dataset_download("thedatasith/hotdog-nothotdog"))
-
     # Search for nested 'train' folder
     train_folders = list(base.rglob("train"))
     test_folders = list(base.rglob("test"))
-
-    assert train_folders, "No train folder found!"
-    assert test_folders, "No test folder found!"
-
     return train_folders[0], test_folders[0]
 
 
@@ -28,36 +26,33 @@ def get_cat_path():
 
 def load_cat_data():
     cat_dir = Path(get_cat_path())
-
-    train_file = cat_dir / "train_catsvsnoncats.h5"
-    test_file = cat_dir / "test_catsvsnoncats.h5"
-
+    train_file = cat_dir / "train_catvsnoncat.h5"
+    test_file = cat_dir / "test_catvsnoncat.h5"
     with h5py.File(train_file, "r") as f:
         train_x = np.array(f["train_set_x"][:])  # shape (m, 64, 64, 3)
         train_y = np.array(f["train_set_y"][:])  # shape (m,)
-
     with h5py.File(test_file, "r") as f:
         test_x = np.array(f["test_set_x"][:])
         test_y = np.array(f["test_set_y"][:])
 
     return train_x, train_y, test_x, test_y
 
-from torchvision import datasets, transforms
-from torch.utils.data import DataLoader
+def load_cat_data_images():
+    train_x, train_y, test_x, test_y = load_cat_data()
 
-transform = transforms.Compose([
-    transforms.Resize((224, 224)),  # for ResNet18
-    transforms.ToTensor()
-])
+    # train_y == 1 → cat (label = 2), 0 → not cat (label = 0)
+    def convert(images, labels):
+        data = []
+        for img_arr, label in zip(images, labels):
+            img = Image.fromarray(img_arr)  # shape (64, 64, 3), uint8
+            label_mapped = 2 if label == 1 else 0
+            data.append((img, label_mapped))
+        return data
 
+    train_data = convert(train_x, train_y)
+    test_data = convert(test_x, test_y)
+    return train_data, test_data
 
-hotdog_train_path, hotdog_test_path = get_hotdog_train_test_paths()
-
-hotdog_train_ds = datasets.ImageFolder(hotdog_train_path, transform=transform)
-hotdog_test_ds = datasets.ImageFolder(hotdog_test_path, transform=transform)
-
-hotdog_train_loader = DataLoader(hotdog_train_ds, batch_size=32, shuffle=True)
-hotdog_test_loader = DataLoader(hotdog_test_ds, batch_size=32)
 
 class CatDataset(Dataset):
     def __init__(self, images, label, transform=None):
@@ -75,22 +70,55 @@ class CatDataset(Dataset):
             img = self.transform(img)
         return img, self.label
     
-from torchvision.datasets import ImageFolder
+class_names = ['neither', 'hotdog', 'cat']
+class_to_idx = {'nothotdog': 0, 'hotdog': 1, 'cat': 2}
 
-class RelabelledFolder(ImageFolder):
-    def __init__(self, root, transform, relabel_map):
-        super().__init__(root=root, transform=transform)
-        self.relabel_map = relabel_map
+
+class CombinedHotdogCatDataset(Dataset):
+    def __init__(self, hotdog_dir, cat_data, transform=None):
+        self.samples = []
+        self.transform = transform
+
+        # Load hotdog dataset from folders
+        class_map = {'nothotdog': 0, 'hotdog': 1}
+        for cls in ['nothotdog', 'hotdog']:
+            class_folder = os.path.join(hotdog_dir, cls)
+            for fname in os.listdir(class_folder):
+                if fname.lower().endswith(('.jpg', '.jpeg', '.png')):
+                    path = os.path.join(class_folder, fname)
+                    label = class_map[cls]
+                    self.samples.append((path, label))
+
+        # Append cat dataset (already (PIL.Image, label))
+        self.samples.extend(cat_data)
+
+    def __len__(self):
+        return len(self.samples)
 
     def __getitem__(self, idx):
-        img, label = super().__getitem__(idx)
-        label = self.relabel_map[self.classes[label]]
-        return img, label
-from torch.utils.data import ConcatDataset
+        item, label = self.samples[idx]
+        if isinstance(item, str):  # a filepath
+            image = Image.open(item).convert('RGB')
+        else:  # already a PIL image from HDF5
+            image = item
 
-def get_combined_dataset():
-    transform = transforms.Compose([
-        transforms.Resize((224, 224)),
-        transforms.ToTensor()
-    ])
+        if self.transform:
+            image = self.transform(image)
+
+        return image, label
+
+
+hotdog_train_path, hotdog_test_path = get_hotdog_train_test_paths()
+cat_train_data, cat_test_data = load_cat_data_images()
+
+transform = transforms.Compose([
+    transforms.Resize((224, 224)),
+    transforms.ToTensor()
+])
+
+train_dataset = CombinedHotdogCatDataset(hotdog_train_path, cat_train_data, transform)
+test_dataset = CombinedHotdogCatDataset(hotdog_test_path, cat_test_data, transform)
+
+train_loader = DataLoader(train_dataset, batch_size=32, shuffle=True)
+test_loader = DataLoader(test_dataset, batch_size=32)
 
--- testingExistingModel.py
+++ testingExistingModel.py
@@ -2,11 +2,10 @@ import torch
 import torch.nn as nn
 from torchvision import models
 from torch.utils.data import Dataset
-from hotdog import train_loader, test_loader
+from datasets import train_loader, test_loader
 
 # Load pretrained ResNet18
 ResNet = models.resnet18(weights="DEFAULT")
-
 # Move to device
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 ResNet.to(device)
@@ -16,6 +15,7 @@ correct = 0
 total = 0
 
 hotdog_imagenet_class = 934
+cat__imagenet_classes = {281, 282, 283, 284, 285}
 
 with torch.no_grad():
     for images, labels in test_loader:
@@ -24,9 +24,18 @@ with torch.no_grad():
         
         outputs = ResNet(images)
         _, predicted = torch.max(outputs, 1)
-        is_hotdog = (predicted == hotdog_imagenet_class).long()  # 1 if hotdog
-        correct += (is_hotdog == labels).sum().item()
+        predicted_class = []
+        for pred in predicted.tolist():
+            if pred == hotdog_imagenet_class:
+                predicted_class.append(1)
+            elif pred in cat__imagenet_classes:
+                predicted_class.append(2)
+            else:
+                predicted_class.append(0)
+
+        predicted_class = torch.tensor(predicted_class).to(device)
+        correct += (predicted_class == labels).sum().item()
         total += labels.size(0)
 
 accuracy = 100 * correct / total
-print(f"Zero-shot hotdog accuracy: {accuracy:.2f}%")
+print(f"Zero-shot 3-class accuracy using ImageNet logits: {accuracy:.2f}%")
